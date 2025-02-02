07cd0f02ebb80008aec4ffbaf20ed752e5ceafe4
BjornTheThunder <simonebonso5@gmail.com>
2025-02-02 21:19:58 +0100

    Added the app

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # simple_chat_app
 Simple chat app made for showing my learning with React and Fullstack development
+
+## Technologies used
+- Made with the MERN Stack: MongoDB, Express, React, Node.js
+- socket.io for realtime chat communication
+- TailwindCSS and DaisyUI for the beautiful interface and themes support
+- Cloudify for managing the profile photo and the ability to send images in chat
+
+## How to use
+Modify the .env file in the backend folder to set all the string connections with the MongoDB database and with the Cloudinary service.
+Then inside the "backend" and "frontend" folders each type the command "npm i" for installing all the relevant packages (Need to have installed Node.js). After that, you can start the application by typing "npm run dev" in each main folder: frontend and backend folders.
---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,69 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
+
+export const getUsersFromSidebar = async (req, res) => {
+    try {
+        const loggedInUserId = req.user._id;
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+
+        res.status(200).json(filteredUsers);
+    } catch (e) {
+        console.log("Error in getUsersFromSidebar", e.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const getMessages = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, receiverId: userToChatId },
+                { senderId: userToChatId, receiverId: myId }
+            ]
+        })
+
+        res.status(200).json(messages);
+    } catch (e) {
+        console.log("Error in getMessages", e.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const myId = req.user._id;
+
+        let imageUrl;
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId: myId,
+            receiverId: receiverId,
+            text: text,
+            image: imageUrl
+        });
+
+        await newMessage.save();
+
+        //realtime functionality => socket.io
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if(receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
+
+        res.status(201).json(newMessage);
+    } catch (e) {
+        console.log("Error in sendMessage controller", e.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
